7d2b0805952d4700447c7f6edcec7fb6b8027774
Lucas-Almeida729 <lucas.almeida729@gmail.com>
2025-10-07 15:15:35 -0300

    Preparar para deploy - remover SQLite e adicionar PostgreSQL

--- src/lib/mock-data.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/mock-data.ts
-import { Book } from '@/types/book'
-
-// CORREÇÃO: Trocar 'let' por 'const'
-export const initialBooks: Book[] = [
-  // ... (conteúdo do array, se houver)
-]
